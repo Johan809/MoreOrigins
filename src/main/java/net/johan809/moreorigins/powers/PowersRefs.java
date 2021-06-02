e596b93147b56f29d7e606ca9674a6ba3cb9b742
Johan <60815230+Johan809@users.noreply.github.com>
2021-06-02 08:00:07 -0400

    feat: declared Wither_Flight var

--- src/main/java/net/johan809/moreorigins/powers/PowersRefs.java
+++ src/main/java/net/johan809/moreorigins/powers/PowersRefs.java
@@ -9,10 +9,10 @@ import net.johan809.moreorigins.MoreOrigins;
 public class PowersRefs {
     public static final PowerType<Power> NIGHT_MONSTER = new PowerTypeReference<>(
             new Identifier(MoreOrigins.MODID, "night_monster"));
-    /*public static final PowerType<Power> MAGIC_RESISTANCE = new PowerTypeReference<>(
-    		new Identifier(MoreOrigins.MODID, "magic-resistance")); */
     public static final PowerType<Power> HOSTILE_BEING = new PowerTypeReference<>(
     		new Identifier(MoreOrigins.MODID, "hostile_being"));
     public static final PowerType<Power> WITHER_AMOR = new PowerTypeReference<>(
     		new Identifier(MoreOrigins.MODID, "wither_armor"));
+    public static final PowerType<Power> WITHER_FLIGHT = new PowerTypeReference<>(
+            new Identifier(MoreOrigins.MODID, "wither_flight"));
 }
